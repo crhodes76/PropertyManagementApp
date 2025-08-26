2743c059367fe66e70cd42366198b842ce9fd149
Charles Rhodes <crhodes76@users.noreply.github.com>
2025-08-26 11:27:31 -0400

    Shell initial development Mdoel View Controller application

---
+++ PropertyManagementApplication/PropertyManagementApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PropertyManagementApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
